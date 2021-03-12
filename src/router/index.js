2e9dd204bdf2ef4b18f7a6d5de99ba706c48b20c
Lima <dlimma@gmail.com>
2021-03-12 14:48:53 -0300

    consumo de api

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Classificacao from '../views/Classificacao.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +14,7 @@ const routes = [
   {
     path: '/classificacao',
     name: 'Classificacao',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Classificacao.vue')
+    component: Classificacao
   }
 ]
 
